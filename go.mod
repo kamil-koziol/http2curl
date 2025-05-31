5012d739fd0afe5fac7cfc0c31e3c2e64dbda80f
Kamil Koziol <koziol.kamil@protonmail.com>
2025-05-31 18:42:21 +0200

    feat(convert): simple convert .http to curl

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kamil-koziol/http2curl
 
 go 1.24.2
+
+require github.com/kamil-koziol/restree v0.0.0-20250531163301-3557667aac96
---
+++ pkg/http2curl/convert.go
@@ -0,0 +1,28 @@
+package http2curl
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/kamil-koziol/restree/pkg/httpparser"
+)
+
+func Convert(body io.Reader, args ...string) (string, error) {
+	req, err := httpparser.Parse(body)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse .http: %s\n", err)
+	}
+
+	args = append(args, "-X", req.Method)
+	for k, v := range req.Headers {
+		args = append(args, "-H", fmt.Sprintf("'%s: %s'", k, v))
+	}
+	if req.Body != "" {
+		args = append(args, "-d", fmt.Sprintf("'%s'", req.Body))
+	}
+	args = append(args, req.URL.String())
+
+	curl := exec.Command("curl", args...)
+	return curl.String(), nil
+}
